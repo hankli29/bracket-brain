23b8abe975f38e0e1e3d848e8ba207ea1796f0b9
hankli29 <hankli3000@gmail.com>
2026-03-23 14:42:10 -0400

    Set up API call and store data in json format

--- fetch_data.py
+++ fetch_data.py
@@ -10,3 +10,12 @@ load_dotenv() # makes everything in the .env file visible to program
 # access .env variables (api keys)
 odds_key = os.getenv("ODDS_API_KEY")
 kaggle_key = os.getenv("KAGGLE_API_TOKEN")
+
+# get() method returns a response object (a package containing data, headers, status code)
+# url must be entered as a string
+response = requests.get(f"https://api.the-odds-api.com/v4/sports/basketball_ncaab/odds?apiKey={odds_key}&regions=us&markets=h2h&oddsFormat=american")
+print(f"Requests remaining: {response.headers['x-requests-remaining']}")
+print(f"API call status code: {response.status_code}\n")
+
+odds_data = response.json()
+print(odds_data)
