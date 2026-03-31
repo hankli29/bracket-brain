a2fe63b33cc0e9ec783508de7c4de8e8c47996fd
hankli29 <hankli3000@gmail.com>
2026-03-31 19:34:59 -0400

    Import and train model, make predictions, compare predictions with true outcomes for accuracy

--- model.py
+++ model.py
@@ -1,5 +1,25 @@
-import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 import xgboost as xgb
 import pandas as pd
 
 historical_data = pd.read_csv("/Users/hankli/bracket-brain/historical_data.csv")
+
+# drop method returns data frame excluding specified rows/cols (axis defaults to 0 (rows))
+historical_stats = historical_data.drop("WINNER", axis=1)
+historical_outcomes = historical_data["WINNER"]
+
+# we need to separate data into separate training and testing sets to avoid overfitting
+# train using training set, check accuracy with testing set
+x_train, x_test, y_train, y_test = train_test_split(historical_stats, historical_outcomes, test_size = 0.2)
+
+# creates the model
+model = xgb.XGBClassifier()
+# train the model using training data -> xgboost handles training
+model.fit(x_train, y_train)
+# have the model make predictions on the test data
+predictions = model.predict(x_test)
+
+# accuracy_score calculates the accuracy of the model's predictions
+accuracy = accuracy_score(y_test, predictions)
+print(f"Model accuracy: {accuracy:.2f}")
