d7676717963ed42c2aae28cf6b68020eeec06c45
Hank Li <hankli3000@gmail.com>
2026-06-20 18:24:29 -0500

    clean documentation, set random_state, fix parameters

--- scripts/model.py
+++ scripts/model.py
@@ -7,44 +7,33 @@ import mlflow
 import mlflow.xgboost
 from pathlib import Path
 
-# path of project root
-# __file__ contains the absolute path of the current file specific to the machine running the file
 base_dir = Path(__file__).resolve().parent.parent
 
 historical_data = pd.read_csv(base_dir / "data" / "historical_data.csv")
 
-# drop method returns data frame excluding specified rows/cols (axis defaults to 0 (rows))
 historical_stats = historical_data.drop("WINNER", axis=1)
 historical_outcomes = historical_data["WINNER"]
 
-# we need to separate data into separate training and testing sets to avoid overfitting
-# train using training set, check accuracy with testing set
 x_train, x_test, y_train, y_test = train_test_split(historical_stats, historical_outcomes, test_size = 0.2, random_state=42)
 
-# set training parameters for xgboost
 params = {
     "n_estimators": 100,
     "max_depth": 4,
     "learning_rate": 0.05,
-    "features": ", ".join(historical_stats.columns)
 }
 
 mlflow.set_experiment("bracketbrain")
 
 with mlflow.start_run():
-    # creates the model with specified training parameters
-    # "**params" unpacks the entire params dict
-    model = xgb.XGBClassifier(**params)
+    model = xgb.XGBClassifier(**params, random_state=42)
 
-    # train the model using training data -> xgboost handles training
     model.fit(x_train, y_train)
 
-    # have the model make predictions on the test data
     predictions = model.predict(x_test)
-    # accuracy_score calculates the accuracy of the model's predictions
     accuracy = accuracy_score(y_test, predictions)
 
     mlflow.log_params(params)
+    mlflow.log_param("features", ", ".join(historical_stats.columns))
     mlflow.log_metric("accuracy", accuracy)
     mlflow.xgboost.log_model(model, name="model")
 
